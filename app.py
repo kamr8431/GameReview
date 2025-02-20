ee3787c3fa61ddea2bbba59fbc496ce475844014
kamr8431 <rishikamath09@gmail.com>
2025-02-19 21:43:49 -0800

    print files for testing

--- app.py
+++ app.py
@@ -10,17 +10,21 @@ def home():
 
 @app.route('/review',methods = ['POST'])
 def review():
-    username = request.form.get('username')
-    link = request.form.get('game_link')
     ''' try:
         review = GameReview(username)
         results = review.gameReview(link)
     except Exception:
         return render_template('error.html')'''
-    review = GameReview(username)
-    results = review.gameReview(link)
-    evals = results.pop()
-    positions = results.pop()
+
+# Use the current working directory as the starting point
+    print(os.getcwd())
+# Use the current working directory as the starting point
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            full_path = os.path.join(root, file)  # Combine the root directory with the file name
+            print(full_path)
+    username = request.form.get('username')
+    link = request.form.get('game_link')
     return render_template('review.html', results = results, positions = positions, evals = evals)
 
 if __name__ == "__main__":
